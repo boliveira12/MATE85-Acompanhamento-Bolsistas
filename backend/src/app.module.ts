e473429fb9453462483edc8c2972da9a11a4a45d
Breno Rosas <breno.rosas@reidopitaco.com>
2022-09-04 10:25:38 -0300

    feat: email-service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ require('dotenv');
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { StudentsModule } from './students/students.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot(), StudentsModule],
+  imports: [DatabaseModule, ConfigModule.forRoot(), StudentsModule, EmailModule],
   controllers: [],
   providers: [],
 })
